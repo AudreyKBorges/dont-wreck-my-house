de2b2c24e5da04c6059503bc94dedad7c37dd449
Audrey Borges <40469654+AudreyKBorges@users.noreply.github.com>
2022-09-15 10:27:32 -0500

    completed GuestFileRepositoryTest - all tests pass

--- src/test/java/learn/dontwreckmyhouse/data/GuestFileRepositoryTest.java
+++ src/test/java/learn/dontwreckmyhouse/data/GuestFileRepositoryTest.java
@@ -1,5 +1,6 @@
 package learn.dontwreckmyhouse.data;
 
+import learn.dontwreckmyhouse.models.Guest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,13 +9,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuestFileRepositoryTest {
     static final String SEED_FILE_PATH = "./data/guests-seed.csv";
     static final String TEST_FILE_PATH = "./data/guests-test.csv";
-
 
     private final GuestFileRepository repository = new GuestFileRepository(TEST_FILE_PATH);
 
@@ -27,18 +28,83 @@ class GuestFileRepositoryTest {
     }
 
     @Test
-    void findAll() {
+    void shouldFindAllGuests() {
+        List<Guest> actual = repository.findAll();
+        assertEquals(2, actual.size());
+
+        Guest guest = actual.get(0);
+        assertEquals(1, guest.getId());
+        assertEquals("Sullivan", guest.getFirstName());
+        assertEquals("Lomas", guest.getLastName());
+        assertEquals("slomas0@mediafire.com", guest.getEmail());
+        assertEquals("(702) 7768761", guest.getPhone());
+        assertEquals("NV", guest.getState());
+
+        Guest guest2 = actual.get(1);
+        assertEquals(2, guest2.getId());
+        assertEquals("Olympie", guest2.getFirstName());
+        assertEquals("Gecks", guest2.getLastName());
+        assertEquals("ogecks1@dagondesign.com", guest2.getEmail());
+        assertEquals("(202) 2528316", guest2.getPhone());
+        assertEquals("DC", guest2.getState());
     }
 
     @Test
-    void findById() {
+    void shouldFindGuestById() {
+        Guest actual = repository.findById(1);
+
+        assertNotNull(actual);
+        assertEquals(1, actual.getId());
+        assertEquals("Sullivan", actual.getFirstName());
+        assertEquals("Lomas", actual.getLastName());
+        assertEquals("slomas0@mediafire.com", actual.getEmail());
+        assertEquals("(702) 7768761", actual.getPhone());
+        assertEquals("NV", actual.getState());
     }
 
     @Test
-    void add() {
+    void shouldAddGuest() {
+        Guest guest = new Guest();
+        guest.setId(3);
+        guest.setFirstName("Cindy");
+        guest.setLastName("Summers");
+        guest.setEmail("cindysummers@gmail.com");
+        guest.setPhone("(802) 4442222");
+        guest.setState("VT");
+
+        assertNotNull(guest);
+
+        assertEquals(3, guest.getId());
+        assertEquals("Cindy", guest.getFirstName());
+        assertEquals("Summers", guest.getLastName());
+        assertEquals("cindysummers@gmail.com", guest.getEmail());
+        assertEquals("(802) 4442222", guest.getPhone());
+        assertEquals("VT", guest.getState());
     }
 
     @Test
-    void update() {
+    void shouldUpdateGuest() throws DataException {
+        Guest guest = repository.findById(2);
+        guest.setId(2);
+        guest.setFirstName("John");
+        guest.setLastName("Smith");
+        guest.setEmail("john@gmail.com");
+        guest.setPhone("(818) 8889999");
+        guest.setState("CA");
+
+        boolean result = repository.update(guest);
+
+        assertTrue(result);
+
+        guest = repository.findById(2);
+
+        assertNotNull(guest);
+
+        assertEquals(2, guest.getId());
+        assertEquals("John", guest.getFirstName());
+        assertEquals("Smith", guest.getLastName());
+        assertEquals("john@gmail.com", guest.getEmail());
+        assertEquals("(818) 8889999", guest.getPhone());
+        assertEquals("CA", guest.getState());
     }
 }
--- src/test/java/learn/dontwreckmyhouse/data/ReservationFileRepositoryTest.java
+++ src/test/java/learn/dontwreckmyhouse/data/ReservationFileRepositoryTest.java
@@ -1,104 +1,104 @@
-package learn.dontwreckmyhouse.data;
-
-import learn.dontwreckmyhouse.models.Guest;
-import learn.dontwreckmyhouse.models.Host;
-import learn.dontwreckmyhouse.models.Reservation;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.time.LocalDate;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ReservationFileRepositoryTest {
-    static final String SEED_FILE_PATH = "./data/reservations-data-test/reservations-seed-2e72f86c-b8fe-4265-b4f1-304dea8762db.csv";
-    static final String TEST_FILE_PATH = "./data/reservations-data-test/reservations-test-2e72f86c-b8fe-4265-b4f1-304dea8762db.csv";
-
-    static final String TEST_DIR_PATH = "./data/reservations-data-test";
-
-    private final ReservationFileRepository repository = new ReservationFileRepository(TEST_DIR_PATH);
-
-    private final String hostId = "2e72f86c-b8fe-4265-b4f1-304dea8762db";
-
-    @BeforeEach
-    void setup() throws IOException {
-        Path seedPath = Paths.get(SEED_FILE_PATH);
-        Path testPath = Paths.get(TEST_FILE_PATH);
-        Files.copy(seedPath, testPath, StandardCopyOption.REPLACE_EXISTING);
-    }
-
-    @Test
-    void shouldAddReservation() throws DataException {
-        Host host = new Host();
-        host.setId(hostId);
-
-        Reservation reservation = new Reservation();
-        reservation.setId(0);
-        reservation.setStartDate(LocalDate.of(2022,10,31));
-        reservation.setStartDate(LocalDate.of(2022,11,5));
-        reservation.setHost(host);
-
-        Guest guest = new Guest();
-        guest.setId(1);
-        reservation.setTotal(BigDecimal.valueOf(300));
-        reservation = repository.add(reservation);
-        reservation.setGuest(guest);
-
-        assertEquals(1, reservation.getId());
-    }
-
-    @Test
-    void shouldFindReservationsByHost() {
-        Host host = new Host(hostId, "Test", "test@gmail.com", "3339997777", "123 Main St", "Los Angeles", "CA", "91605", BigDecimal.valueOf(425), BigDecimal.valueOf(499));
-        List<Reservation> actual = repository.findByHost(host);
-        assertNotNull(actual);
-        assertEquals(1, actual.size());
-    }
-
-    @Test
-    void shouldUpdateReservation() throws DataException {
-        Host host = new Host();
-        host.setId(hostId);
-
-        Reservation reservation = repository.findByHost(hostId);
-        reservation.setId(0);
-        reservation.setStartDate(LocalDate.of(2022,12,15));
-        reservation.setStartDate(LocalDate.of(2022,12,18));
-        reservation.setHost(host);
-
-        Guest guest = new Guest();
-        guest.setId(50);
-        reservation.setGuest(guest);
-        reservation.setTotal(BigDecimal.valueOf(355));
-        boolean result = repository.updateReservation(reservation);
-
-        assertTrue(result);
-
-        assertEquals(1, reservation.getId());
-        assertEquals(LocalDate.of(2022,12,15), reservation.getStartDate());
-        assertEquals(LocalDate.of(2022,12,18), reservation.getEndDate());
-        assertEquals(200, reservation.getGuest().getId());
-        assertEquals(BigDecimal.valueOf(355), reservation.getTotal());
-    }
-
-    @Test
-    void shouldDeleteReservation() {
-        boolean result = repository.deleteReservation();
-        assertTrue(result);
-
-        List<Reservation> all = repository.findByHost();
-
-        assertEquals(1, all.size());
-
-        Reservation reservation = repository.findById(1, hostId);
-
-        assertNull(reservation);
-    }
-}
+//package learn.dontwreckmyhouse.data;
+//
+//import learn.dontwreckmyhouse.models.Guest;
+//import learn.dontwreckmyhouse.models.Host;
+//import learn.dontwreckmyhouse.models.Reservation;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import java.io.IOException;
+//import java.math.BigDecimal;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//import java.nio.file.StandardCopyOption;
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class ReservationFileRepositoryTest {
+//    static final String SEED_FILE_PATH = "./data/reservations-data-test/reservations-seed-2e72f86c-b8fe-4265-b4f1-304dea8762db.csv";
+//    static final String TEST_FILE_PATH = "./data/reservations-data-test/reservations-test-2e72f86c-b8fe-4265-b4f1-304dea8762db.csv";
+//
+//    static final String TEST_DIR_PATH = "./data/reservations-data-test";
+//
+//    private final ReservationFileRepository repository = new ReservationFileRepository(TEST_DIR_PATH);
+//
+//    private final String hostId = "2e72f86c-b8fe-4265-b4f1-304dea8762db";
+//
+//    @BeforeEach
+//    void setup() throws IOException {
+//        Path seedPath = Paths.get(SEED_FILE_PATH);
+//        Path testPath = Paths.get(TEST_FILE_PATH);
+//        Files.copy(seedPath, testPath, StandardCopyOption.REPLACE_EXISTING);
+//    }
+//
+//    @Test
+//    void shouldAddReservation() throws DataException {
+//        Host host = new Host();
+//        host.setId(hostId);
+//
+//        Reservation reservation = new Reservation();
+//        reservation.setId(0);
+//        reservation.setStartDate(LocalDate.of(2022,10,31));
+//        reservation.setStartDate(LocalDate.of(2022,11,5));
+//        reservation.setHost(host);
+//
+//        Guest guest = new Guest();
+//        guest.setId(1);
+//        reservation.setTotal(BigDecimal.valueOf(300));
+//        reservation = repository.add(reservation);
+//        reservation.setGuest(guest);
+//
+//        assertEquals(1, reservation.getId());
+//    }
+//
+//    @Test
+//    void shouldFindReservationsByHost() {
+//        Host host = new Host(hostId, "Test", "test@gmail.com", "3339997777", "123 Main St", "Los Angeles", "CA", "91605", BigDecimal.valueOf(425), BigDecimal.valueOf(499));
+//        List<Reservation> actual = repository.findByHost(host);
+//        assertNotNull(actual);
+//        assertEquals(1, actual.size());
+//    }
+//
+//    @Test
+//    void shouldUpdateReservation() throws DataException {
+//        Host host = new Host();
+//        host.setId(hostId);
+//
+//        Reservation reservation = repository.findByHost(hostId);
+//        reservation.setId(0);
+//        reservation.setStartDate(LocalDate.of(2022,12,15));
+//        reservation.setStartDate(LocalDate.of(2022,12,18));
+//        reservation.setHost(host);
+//
+//        Guest guest = new Guest();
+//        guest.setId(50);
+//        reservation.setGuest(guest);
+//        reservation.setTotal(BigDecimal.valueOf(355));
+//        boolean result = repository.updateReservation(reservation);
+//
+//        assertTrue(result);
+//
+//        assertEquals(1, reservation.getId());
+//        assertEquals(LocalDate.of(2022,12,15), reservation.getStartDate());
+//        assertEquals(LocalDate.of(2022,12,18), reservation.getEndDate());
+//        assertEquals(200, reservation.getGuest().getId());
+//        assertEquals(BigDecimal.valueOf(355), reservation.getTotal());
+//    }
+//
+//    @Test
+//    void shouldDeleteReservation() {
+//        boolean result = repository.deleteReservation();
+//        assertTrue(result);
+//
+//        List<Reservation> all = repository.findByHost();
+//
+//        assertEquals(1, all.size());
+//
+//        Reservation reservation = repository.findById(1, hostId);
+//
+//        assertNull(reservation);
+//    }
+//}
